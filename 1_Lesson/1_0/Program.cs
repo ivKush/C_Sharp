44592f2c78ab926eaf3cf2c25228781647019ccb
Иван Кушнир <kushnir82@mail.ru>
2022-11-21 21:34:51 +0300

    Задача_1

--- 1_Lesson/1_0/Program.cs
+++ 1_Lesson/1_0/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("Write a number: ");
+
+int num = int.Parse(Console.ReadLine());
+System.Console.WriteLine(num*num);
