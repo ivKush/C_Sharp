e0bd122f781e2068286d1ad8504058f21a554cda
Иван Кушнир <kushnir82@mail.ru>
2022-11-21 22:02:17 +0300

    Задача_3

---
+++ 1_Lesson/3_0/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Введите число");
+int y = int.Parse(Console.ReadLine());
+if ( y == 1) 
+{
+    Console.WriteLine("Понедельник");
+}
+else if (y == 2)
+{
+    Console.WriteLine("Вторник");
+}
+else if (y == 3)
+{
+    Console.WriteLine("Cреда");
+}
+else if (y == 4)
+{
+    Console.WriteLine("Четверг");
+}
+else if (y == 5)
+{
+    Console.WriteLine("Пятница");
+}
+else if (y == 6)
+{
+    Console.WriteLine("Суббота");
+}
+else if (y == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
+else
+{
+    Console.WriteLine("error");
+}
