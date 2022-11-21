a1f7589e9b6a63e71723c936a0766d3f0dcabb9f
Иван Кушнир <kushnir82@mail.ru>
2022-11-21 22:38:34 +0300

    Задача_2 исправление

--- 1_Lesson/2_0/Program.cs
+++ 1_Lesson/2_0/Program.cs
@@ -1,15 +1,17 @@
-﻿System.Console.WriteLine("Введите число 1");
-int num = int.Parse(Console.ReadLine());
+﻿// 1. Напишите программу, которая на вход принимает два числа и проверяет,
+//    являетcя ли первое число квадраом второго
 
-System.Console.WriteLine("Введите число 2");
-int num = int.Parse(Console.ReadLine());
 
-if (num2 == num1*num1);
-   {
-     System.Console.WriteLine("Да");
-   }
-   
-else
-    {
-    System.Console.WriteLine("Нет");
-    }
+Console.WriteLine("Введите число 1");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число 2");
+int num2 = int.Parse(Console.ReadLine());
+
+if(num2 == num1*num1)
+{
+    Console.WriteLine("да");
+}
+
+else 
+    Console.WriteLine("нет");
