edfaa089d07101eb42cb08c16f44a451da9601e0
Иван Кушнир <kushnir82@mail.ru>
2022-11-21 22:00:03 +0300

    Задача_2

---
+++ 1_Lesson/2_0/Program.cs
@@ -0,0 +1,15 @@
+﻿System.Console.WriteLine("Введите число 1");
+int num = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine("Введите число 2");
+int num = int.Parse(Console.ReadLine());
+
+if (num2 == num1*num1);
+   {
+     System.Console.WriteLine("Да");
+   }
+   
+else
+    {
+    System.Console.WriteLine("Нет");
+   }
