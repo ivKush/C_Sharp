90c05aa9c2501670c67cce80682e89618f82805f
Иван Кушнир <kushnir82@mail.ru>
2022-11-22 19:52:54 +0300

    дз 1 Семинара задача 3

--- 1_Lesson_Home/Task_3/Program.cs
+++ 1_Lesson_Home/Task_3/Program.cs
@@ -7,7 +7,8 @@
 Console.WriteLine("Введите число и узнайте четное ли оно: ");
 var number1 = Console.ReadLine();
 int a = int.Parse(number1);
-a/2=b;
-if (b==int) System.Console.WriteLine("чётное");
+int b=a%2;
+if (b==0) System.Console.WriteLine("чётное");
+
 else System.Console.WriteLine("не чётное");
 
