2ea35face2edb768be834111908737ce89cc78c8
Иван Кушнир <kushnir82@mail.ru>
2022-11-22 13:07:55 +0300

    дз 1 Семинара задача 1

---
+++ 1_Lesson_Home/Task_1/Program.cs
@@ -0,0 +1,25 @@
+﻿//Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, 
+//какое число большее, а какое меньшее.
+
+    //a = 5; b = 7 -> max = 7
+    //a = 2 b = 10 -> max = 10
+    //a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Введите 1 число: ");
+var c = Console.ReadLine();
+
+Console.WriteLine("Введите 2 число: ");
+var d = Console.ReadLine();
+
+int a = int.Parse(c);
+int b = int.Parse(d);
+int max = 0;
+
+if (a>b)  
+    {
+    max=a;
+    }
+else max=b;
+
+System.Console.Write("Максимальное число: ");
+Console.Write(max);
