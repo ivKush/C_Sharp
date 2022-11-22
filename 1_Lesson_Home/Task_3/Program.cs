66a6f2997f405b79d09a7c2213e67d1855128386
Иван Кушнир <kushnir82@mail.ru>
2022-11-22 19:03:24 +0300

    дз 1 Семинара задача 3 (незавршена)

---
+++ 1_Lesson_Home/Task_3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 3: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.WriteLine("Введите число и узнайте четное ли оно: ");
+var number1 = Console.ReadLine();
+int a = int.Parse(number1);
+a/2=b;
+if (b==int) System.Console.WriteLine("чётное");
+else System.Console.WriteLine("не чётное");
+
