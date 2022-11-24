d025afe727e49d14b4542806d78f5bac16415933
Иван Кушнир <kushnir82@mail.ru>
2022-11-24 22:12:32 +0300

    промеж.сохр.

---
+++ 2_Lesson/2_0/Program.cs
@@ -0,0 +1,20 @@
+﻿//   Напишите программу, которая выводит случайное
+//    трехзначное число и на выходе показывает
+//     последнюю цифру этого числа.
+
+int Num(int num);
+
+{
+return num %10;
+}
+System.Console.WriteLine(Num);
+System.Console.WriteLine(Num(new Random().Next(100,1000)));
+
+
+//int Num(int num)
+//{
+
+//    return num %10;
+//}
+
+//Console.WriteLine(Num(new Random().Next(100,1000)));
---
+++ 2_Lesson/3_0/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая будет принимать на
+// вход два числа и выводить, является ли второе число
+// кратным первому. Если число 2 не кратно числу 1, то
+// программа выводит остаток от деления.
+
+string crat(int num,int num2)
+{
+    if (num % num2 == 0)
+        return "кратно";
+    else
+        return $"некратно, остаток = {num % num2}";
+}
+Console.WriteLine("1 chislo");
+int first = int.Parse(Console.ReadLine());
+Console.WriteLine("2 chislo");
+int second = int.Parse(Console.ReadLine());
+Console.WriteLine(Crat(first, second));
+
