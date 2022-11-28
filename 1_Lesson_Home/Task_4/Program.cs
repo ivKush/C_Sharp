b0ea753b1e4b130ce31707a76ab14a920763186f
Иван Кушнир <kushnir82@mail.ru>
2022-11-28 19:05:59 +0300

    исправление Task_4

--- 1_Lesson_Home/Task_4/Program.cs
+++ 1_Lesson_Home/Task_4/Program.cs
@@ -3,12 +3,25 @@
 //5 -> 2, 4
 //8 -> 2, 4, 6, 8
 
-Console.Write("Введите число от 1: ");
-int N =int.Parse(Console.ReadLine());
+//Console.Write("Введите число от 1: ");
+//int N =int.Parse(Console.ReadLine());
 
-System.Console.Write("Все чётные числа: ");   
-for (int a=1, b=0; a<=N; a++) 
+//System.Console.Write("Все чётные числа: ");   
+//for (int a=1, b=0; a<=N; a++) 
+//{
+    //b=a%2;
+  //  if (b==0) System.Console.Write($"{a} ");
+//}
+
+Console.Write("Введите число: ");
+
+int N = int.Parse(Console.ReadLine());
+int a = 1;
+while (a<=N) 
 {
-    b=a%2;
-    if (b==0) System.Console.Write(-a);
+    if (a % 2 == 0)
+    
+       Console.Write($"{a} ");
+    
+    a++;
 }
