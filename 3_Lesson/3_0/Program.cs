4f7258c53cb5b7d9b4091e9ad0293e004dbe7499
Иван Кушнир <kushnir82@mail.ru>
2022-11-29 11:28:16 +0300

    задача 3

---
+++ 3_Lesson/3_0/Program.cs
@@ -0,0 +1,23 @@
+﻿// // 2. Напишите программу, которая принимает на вход координаты
+// двух точек и находит расстояние между ними в 2D пространстве.
+
+int ReadNumber(string message)
+{
+Console.Write(message);
+var s = Console.ReadLine();
+int a = s == null ? 0 : int.Parse(s);
+return a;
+}
+double Distance(int ax, int ay, int bx, int by)
+{
+double result;
+result = Math.Sqrt(Math.Pow(bx - ax, 2) + Math.Pow(by - ay, 2));
+return result;
+}
+
+int ax = ReadNumber("Введите aX:");
+int ay = ReadNumber("Введите aY:");
+int bx = ReadNumber("Введите bX:");
+int by = ReadNumber("Введите bY:");
+
+Console.WriteLine(Math.Round(Distance(ax, ay, bx, by), 2));
