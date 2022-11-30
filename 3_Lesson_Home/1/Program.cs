f38acba8ad0f37abe216f85581ba7e7a16c1a8e1
Иван Кушнир <kushnir82@mail.ru>
2022-11-30 21:20:33 +0300

    ДЗ задача 1

---
+++ 3_Lesson_Home/1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+System.Console.Write("Ввведите пятизначное число: ");
+var empty = Console.ReadLine();
+int number = empty == null ? 0 : int.Parse(empty);
+string a;
+if (number % 10 == number / 10000 && (number / 10) % 10 == (number / 1000) % 10) a = "ДА"; else a = "НЕТ";
+if (number > 99999 || number < 10000) a = "Error";
+
+System.Console.WriteLine(a);
+
+
+// System.Console.Write("Ввведите пятизначное число: ");
+// int number = int.Parse(Console.ReadLine());
+// string a;
+// if (number % 10 == number / 10000 && (number / 10) % 10 == (number / 1000) % 10)
+// {
+//     a = "ДА";
+// }
+// else a = "НЕТ";
+// if (number > 99999 || number < 10000)
+// {
+//     a = "Error";
+// }
+// System.Console.WriteLine(a);
---
+++ 3_Lesson_Home/3/Program.cs
@@ -0,0 +1,27 @@
+﻿// 3_Lesson_Home_1
+ System.Console.WriteLine("Ввведите пятизначное число");
+    var empty = Console.ReadLine();
+    int number = empty == null ? 0 : int.Parse(empty);
+
+string palindrome( string a)
+{
+   
+    //string a;
+    if (number > 99999 || number < 10000)
+    {
+        a = "Error";
+    }
+        if (number % 10 == number / 10000)
+        {
+        a = "ДА";
+        }
+        if ((number / 10) % 10 == (number / 1000) % 10)
+        {
+        a = "ДА";
+        }
+        else a = "НЕТ";
+    
+    return a;
+}
+palindrome(number, a);
+//System.Console.WriteLine(palindrome);
