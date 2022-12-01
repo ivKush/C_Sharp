63d38c5d032cd14cae959171f53fa2487c1baf54
Иван Кушнир <kushnir82@mail.ru>
2022-12-01 18:06:57 +0300

    ДЗ задача 2

--- 3_Lesson_Home/1/Program.cs
+++ 3_Lesson_Home/1/Program.cs
@@ -6,7 +6,7 @@ int number = empty == null ? 0 : int.Parse(empty);
 string a;
 if (number % 10 == number / 10000 && (number / 10) % 10 == (number / 1000) % 10) a = "ДА"; else a = "НЕТ";
 if (number > 99999 || number < 10000) a = "Error";
-
+if (number < 0) a = "НЕТ";
 System.Console.WriteLine(a);
 
 
--- 3_Lesson_Home/2/Program.cs
+++ 3_Lesson_Home/2/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿// Напишите программу, которая принимает на вход координаты двух точек и 
+// находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+
+Console.Write("Введите aX:");
+int ax = int.Parse(Console.ReadLine());
+Console.Write("Введите aY:");
+int ay = int.Parse(Console.ReadLine());
+Console.Write("Введите aZ:");
+int az = int.Parse(Console.ReadLine());
+Console.Write("Введите bX:");
+int bx = int.Parse(Console.ReadLine());
+Console.Write("Введите bY:");
+int by = int.Parse(Console.ReadLine());
+Console.Write("Введите bZ:");
+int bz = int.Parse(Console.ReadLine());
+
+   double result = Math.Sqrt(Math.Pow(bx - ax, 2) + Math.Pow(by - ay, 2) + Math.Pow(bz - az, 2));
+ 
+Console.WriteLine(result);
+//Console.WriteLine(Math.Round(Distance(ax, ay, az, bx, by, bz), 2));
