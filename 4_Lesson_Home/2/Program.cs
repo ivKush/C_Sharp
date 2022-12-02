38f95d49f3a0417fe27bc31836cb9b107418d333
Иван Кушнир <kushnir82@mail.ru>
2022-12-02 16:30:22 +0300

    задача 2

---
+++ 4_Lesson_Home/2/Program.cs
@@ -0,0 +1,10 @@
+﻿//  Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//  452 -> 11
+//  82 -> 10
+//  9012 -> 12
+
+int A = int.Parse(Console.ReadLine());
+
+int c = 0;
+for (int i = 0, b; i < A; b = A % 10, A = A / 10, c = b + c) ;
+System.Console.WriteLine(c);
