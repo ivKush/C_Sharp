45ca7b33eb535741437d2ca30f65b05d74642491
Иван Кушнир <kushnir82@mail.ru>
2022-12-02 10:15:51 +0300

    задачи семинара

---
+++ 4_Lesson/2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+
+int NumbCount(int n)
+{
+if(n == 0)
+{
+return(1);
+}
+int count = 0;
+while (n > 0)
+{
+n = n/10;
+count++;
+}
+return count;
+
+}
+Console.WriteLine(NumbCount(int.Parse(Console.ReadLine())));
