41f148151960e9fcd67002412c436507f6e13098
Иван Кушнир <kushnir82@mail.ru>
2022-12-05 21:00:13 +0300

    задача 2

--- 5_Lesson/2/Program.cs
+++ 5_Lesson/2/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1. Напишите программу замена элементов массива:
+//    положительные элементы замените на соответствующие отрицательные, и наоборот.
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+}
+
+int[] Mass(int size)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(-100, 100);
+    return arr;
+}
+
+void Massiv(int[] arr_1)
+{
+    for (int i = 0;i < arr_1.Length; i++)
+        arr_1[i] = -arr_1[i];
+
+}
+
+int[] arr_1 = Mass(int.Parse(Console.ReadLine()));
+Print(arr_1);
+Massiv(arr_1);
+Print(arr_1);
+
