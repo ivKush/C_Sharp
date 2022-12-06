e30f8c5478455b1cecb850504a9884344d58ae3f
Иван Кушнир <kushnir82@mail.ru>
2022-12-06 14:16:37 +0300

    задачи

---
+++ 5_Lesson/4/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задайте одномерный массив из 123 случайных чисел.
+// Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+}
+
+int[] ArrayNums(int size, int start, int last)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(start, last + 1);
+    return arr;
+}
+
+int CountDiap(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] >= 10 && arr[i] <= 99)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int[] arr_1 = ArrayNums(int.Parse(Console.ReadLine()),
+int.Parse(Console.ReadLine()),
+int.Parse(Console.ReadLine()));
+Print(arr_1);
+Console.Write(CountDiap(arr_1));
---
+++ 5_Lesson/5/Program.cs
@@ -0,0 +1,17 @@
+﻿// Найдите произведение пар чисел в одномерном массиве.
+// Парой считаем первый и последний элемент, второй и предпоследний
+// и т.д. Результат запишите в новом массиве.
+
+int[] PairsNum(int[] arr)
+{
+int size = arr.Length;
+int flex_size = size / 2 + size % 2;
+int[] new_arr = new int[flex_size];
+
+for (int i = 0; i < size / 2; i++)
+new_arr[i] = arr[i] * arr[size - i - 1];
+
+if (new_arr[flex_size - 1] == 0)
+new_arr[flex_size - 1] = arr[flex_size - 1];
+return new_arr;
+}
