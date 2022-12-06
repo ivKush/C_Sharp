66b9e8dd734165e05f27ff8bf56060c6d36a1c2f
Иван Кушнир <kushnir82@mail.ru>
2022-12-06 18:51:09 +0300

    з_2

--- 5_Leeson_Home/2/Program.cs
+++ 5_Leeson_Home/2/Program.cs
@@ -5,3 +5,32 @@
 // [3, 7, 23, 12] -> 26
 // [-4, -6, 4, 67] -> 0
 
+void PrintMass(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+}
+
+int[] CollMass(int size)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(1, 10);
+    return arr;
+}
+
+int NegPosSumm(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; sum = arr[i] + sum, i += 2) ;
+    return sum;
+}
+
+int[] ready = CollMass(int.Parse(Console.ReadLine()));
+PrintMass(ready);
+System.Console.WriteLine(NegPosSumm(ready));
+
