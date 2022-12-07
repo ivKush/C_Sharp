62daa15a8eb041b12a6e19690614298472524fa5
Иван Кушнир <kushnir82@mail.ru>
2022-12-07 11:04:45 +0300

    з_3

--- 5_Leeson_Home/3/Program.cs
+++ 5_Leeson_Home/3/Program.cs
@@ -1,11 +1,45 @@
 ﻿
-//  https://docs.microsoft.com/en-us/dotnet/api/system.random.nextdouble?view=net-6.0
-//  https://ru.stackoverflow.com/questions/1254105/%D0%9A%D0%B0%D0%BA-%D1%81%D0%B3%D0%B5%D0%BD%D0%B5%D1%80%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D1%82%D1%8C-%D1%81%D0%BB%D1%83%D1%87%D0%B0%D0%B9%D0%BD%D0%BE%D0%B5-%D0%B4%D1%80%D0%BE%D0%B1%D0%BD%D0%BE%D0%B5-%D1%87%D0%B8%D1%81%D0%BB%D0%BE-%D0%B8%D0%B7-%D0%B8%D0%BD%D1%82%D0%B5%D1%80%D0%B2%D0%B0%D0%BB%D0%B0
-
-//  дабл - с окрулением до 2-х после запятой.
-
 // Задача 3: Задайте массив вещественных чисел. Найдите
 // разницу между максимальным и минимальным
 // элементов массива.
 
 // [3, 7, 22, 2, 78] -> 76
+
+void PrintMass(double[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]}  ");
+    Console.WriteLine();
+}
+
+double[] CollMass(int size)
+{
+    double[] arr = new double[size];
+    var rand = new Random();
+
+   for (int i = 0; i < size; i++)
+        arr[i] = Math.Round((rand.NextDouble() * (100+100) - 100), 2);
+    return arr;
+}
+
+void Diff(double[] arr)
+{
+    double max = arr[0];
+    double min = arr[0];
+    double diff = 0;
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (arr[i] > max) max = arr[i];
+        if (arr[i] < min) min = arr[i];
+    }
+    diff = max - min;
+    diff = Math.Round(diff, 2);
+    //Math.Round((diff = max - min), 2);
+    System.Console.WriteLine($"({max}) - ({min}) = {diff}");
+}
+double[] ready = CollMass(int.Parse(Console.ReadLine()));
+PrintMass(ready);
+Diff(ready);
+
