247f87c951f268a3a2b0994c7a81da41ef88365c
Иван Кушнир <kushnir82@mail.ru>
2022-12-13 17:55:20 +0300

    ...

--- 6_Lesson_Home/2/Program.cs
+++ 6_Lesson_Home/2/Program.cs
@@ -6,16 +6,16 @@
 void Intersection(double k1, double b1, double k2, double b2)
 {
     double k_sub = k1 - k2;
-    if (k_sub != 0) 
+    if (k_sub != 0)
     {
         double x = (b2 - b1) / (k1 - k2);
         double y = k1 * x + b1;
-        System.Console.WriteLine($"точка пересечения: ({x}; {y})");
+        System.Console.WriteLine($"точка пересечения: ( x = {x} ; y = {y} )");
     }
     else if (k1 == k2 && b1 == b2)
-        System.Console.WriteLine("существует бесконечное число точек пересечения");
+        System.Console.WriteLine("существует много точек пересечения");
     else
-        System.Console.WriteLine("точки пересечения нет");
+        System.Console.WriteLine("нет точки пересечения");
 }
 
 Intersection(int.Parse(Console.ReadLine()),
@@ -25,7 +25,12 @@ Intersection(int.Parse(Console.ReadLine()),
 
 
 
-// int y, k = 5, b = 2, x;
-// y = k * x + b;
-// Console.WriteLine(y);
+
+
+// double x = (-b2 + b1)/(-k1 + k2);
+// double y = k2 * x + b2;
+
+// Console.WriteLine($"две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
+
+
 
