503059c892d54534e3ada8c63124b9f1e0a15887
Иван Кушнир <kushnir82@mail.ru>
2022-12-14 14:13:25 +0300

    ДЗ задание 1

---
+++ 7_lesson_home/1/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 1: Задайте двумерный массив размером m×n,
+// заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+double[,] TwoCollMass(int row, int column)
+{
+    double[,] arr = new double[row, column];
+    var rand = new Random();
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+            arr[i, j] = Math.Round(rand.NextDouble() * (10 + 10) - 10, 1);
+    }
+    return arr;
+}
+
+void PrintMass(double[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            System.Console.Write($"{arr[i, j]}  ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+double[,] arr1 = TwoCollMass(int.Parse(Console.ReadLine()),
+                            int.Parse(Console.ReadLine()));
+PrintMass(arr1);
---
+++ 7_lesson_home/2/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 2: Напишите программу, которая на вход
+// принимает позиции элемента в двумерном массиве, и
+// возвращает значение этого элемента или же указание,
+// что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1 7 -> такого числа в массиве нет
+
+int[,] CollMass(int row, int column)
+{
+    int[,] arr = new int[row, column];
+    var rand = new Random();
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            arr[i, j] = rand.Next(1, 10);
+        }
+    }
+}
+
+void PrintMass(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            System.Console.WriteLine($"{arr[i, j]}  ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+System.Console.WriteLine("Введите позицию элемента: ");
+int [,] itemPosi = CollMass(int.Parse(Console.ReadLine(arr[,])));
