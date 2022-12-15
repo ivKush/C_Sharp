de796d0ff195a7758ada54a2d238909b5519907b
Иван Кушнир <kushnir82@mail.ru>
2022-12-15 17:48:44 +0300

    .

--- 7_lesson/7_4/Program.cs
+++ 7_lesson/7_4/Program.cs
@@ -3,37 +3,37 @@
 
 void Print(int[,] arr)
 {
-int row_size = arr.GetLength(0);
-int column_size = arr.GetLength(1);
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
 
-for (int i = 0; i < row_size; i++)
-{
-for (int j = 0; j < column_size; j++)
-Console.Write($" {arr[i, j],4} ");
-Console.WriteLine();
-}
-Console.WriteLine();
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+            Console.Write($" {arr[i, j],4} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
 
 }
 
 
 int[,] MassNums(int row, int column, int from, int to)
 {
-int[,] arr = new int[row, column];
+    int[,] arr = new int[row, column];
 
-for (int i = 0; i < row; i++)
-for (int j = 0; j < column; j++)
-arr[i, j] = new Random().Next(from, to);
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to);
 
-return arr;
+    return arr;
 }
 string FindElem(int[,] arr, int findNum)
 {
-for (int i = 0; i < arr.GetLength(0); i++)
-for (int j = 0; j < arr.GetLength(1); j++)
-if (arr[i, j] == findNum)
-return $"Искомый элемент {findNum} находится на позиции [{i + 1}, {j + 1}]";
-return $"Искомый элемент {findNum} не найден";
+    for (int i = 0; i < arr.GetLength(0); i++)
+        for (int j = 0; j < arr.GetLength(1); j++)
+            if (arr[i, j] == findNum)
+                return $"Искомый элемент {findNum} находится на позиции [{i + 1}, {j + 1}]";
+    return $"Искомый элемент {findNum} не найден";
 }
 
 Console.Write("Enter the number of rows: ");
